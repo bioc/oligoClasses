17c3e7053552f0fe975c086d30a34671f3cc87ef
Rob Scharp <rscharpf@jhsph.edu>
2007-09-09 10:29:29 +0000

    removed quotes surrounding linkS4Class{eSet}
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@26972 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/assayData-methods.R
+++ R/assayData-methods.R
@@ -15,46 +15,12 @@ setReplaceMethod("copyNumber", signature(object="SnpLevelSet", value="matrix"),
 
 setMethod("cnConfidence", "SnpLevelSet", function(object) assayDataElement(object, "cnConfidence"))
 setReplaceMethod("cnConfidence", signature(object="SnpLevelSet", value="matrix"),
-                 function(object, value) assayDataElementReplace(object, "cnConfidence", value))
-
-
-##SnpCallSet
-##setMethod("calls", "SnpCallSet", function(object) assayDataElement(object, "calls"))
-##setReplaceMethod("calls", signature(object="SnpCallSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "calls", value))
-##
-##setMethod("callsConfidence", "SnpCallSet", function(object) assayDataElement(object, "callsConfidence"))
-##setReplaceMethod("callsConfidence", signature(object="SnpCallSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "callsConfidence", value))
+                 function(object, value){
+                   assayDataElementReplace(object, "cnConfidence", value)
+                 })
 
 setMethod("db", "SnpCallSet",
           function(object) db(get(annotation(object))))
 
 
-##SnpCopyNumberSet
-##setMethod("copyNumber", "SnpCopyNumberSet", function(object) assayDataElement(object, "copyNumber"))
-##setReplaceMethod("copyNumber", signature(object="SnpCopyNumberSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "copyNumber", value))
-##
-##setMethod("cnConfidence", "SnpCopyNumberSet", function(object) assayDataElement(object, "cnConfidence"))
-##setReplaceMethod("cnConfidence", signature(object="SnpCopyNumberSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "cnConfidence", value))
 
-
-##oligoSnpSet
-##setMethod("calls", "oligoSnpSet", function(object) assayDataElement(object, "calls"))
-##setReplaceMethod("calls", signature(object="oligoSnpSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "calls", value))
-##
-##setMethod("callsConfidence", "oligoSnpSet", function(object) assayDataElement(object, "callsConfidence"))
-##setReplaceMethod("callsConfidence", signature(object="oligoSnpSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "callsConfidence", value))
-##
-##
-##setMethod("copyNumber", "oligoSnpSet", function(object) assayDataElement(object, "copyNumber"))
-##setReplaceMethod("copyNumber", signature(object="oligoSnpSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "copyNumber", value))
-##
-##setMethod("cnConfidence", "oligoSnpSet", function(object) assayDataElement(object, "cnConfidence"))
-##setReplaceMethod("cnConfidence", signature(object="oligoSnpSet", value="matrix"),
-##                 function(object, value) assayDataElementReplace(object, "cnConfidence", value))
