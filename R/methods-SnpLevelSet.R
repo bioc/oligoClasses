c2ccf6e8c12db56c82bdb10a08655d17a2d8294a
Rob Scharp <rscharpf@jhsph.edu>
2008-02-20 15:02:31 +0000

    changed the chromosome and position methods for SnpLevelSet -- should work for combined objects
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@30055 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-SnpLevelSet.R
+++ R/methods-SnpLevelSet.R
@@ -1,12 +1,12 @@
-setMethod("chromosome", "SnpLevelSet",
-          function(object){
-            require("RSQLite") || stop("RSQLite package not available")            
-            fs <- featureNames(object)
-            sql <- "SELECT man_fsetid, chrom FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
-            tmp <- dbGetQuery(db(object), sql)
-            idx <- match(fs, tmp[["man_fsetid"]])
-            tmp[idx, "chrom"]
-          })
+##setMethod("chromosome", "SnpLevelSet",
+##          function(object){
+##            require("RSQLite") || stop("RSQLite package not available")            
+##            fs <- featureNames(object)
+##            sql <- "SELECT man_fsetid, chrom FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
+##            tmp <- dbGetQuery(db(object), sql)
+##            idx <- match(fs, tmp[["man_fsetid"]])
+##            tmp[idx, "chrom"]
+##          })
 
 setMethod("db", signature(object="DBPDInfo"),
           function(object) object@getdb())
@@ -17,15 +17,68 @@ setMethod("db", "SnpLevelSet",
             get(annotation(object))@getdb()
         })
 
+setMethod("chromosome", "SnpLevelSet",
+          function(object){
+            if(!("chromosome" %in% fvarLabels(object))){
+              require("RSQLite") || stop("RSQLite package not available")
+              fs <- featureNames(object)
+              sql <- "SELECT man_fsetid, chrom FROM featureSet WHERE man_fsetid LIKE 'SNP%'"              
+              ##Check if two objects have been combined
+              pkgs <- strsplit(annotation(object), ",")[[1]]
+              if(length(pkgs) > 1){
+                object2 <- object1 <- object
+                annotation(object1) <- pkgs[1]
+                annotation(object2) <- pkgs[2]
+
+                tmp1 <- dbGetQuery(db(object1), sql)
+                tmp2 <- dbGetQuery(db(object2), sql)
+                tmp <- rbind(tmp1, tmp2)
+              } else {
+                tmp <- dbGetQuery(db(object), sql)
+              }
+              idx <- match(fs, tmp[["man_fsetid"]])
+              chr <- tmp[idx, "chrom"]
+            } else {
+              chr <- featureData(object)$chromosome
+            }
+            return(chr)
+          })
+
 setMethod("position", "SnpLevelSet",
           function(object){
-            require("RSQLite") || stop("RSQLite package not available")            
-            fs <- featureNames(object)
-            sql <- "SELECT man_fsetid, physical_pos FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
-            tmp <- dbGetQuery(db(object), sql)
-            idx <- match(fs, tmp[["man_fsetid"]])
-            tmp[idx, "physical_pos"]
+            if(!("position" %in% fvarLabels(object))){
+              require("RSQLite") || stop("RSQLite package not available")
+              fs <- featureNames(object)
+              sql <- "SELECT man_fsetid, physical_pos FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
+              pkgs <- strsplit(annotation(object), ",")[[1]]
+              if(length(pkgs) > 1){
+                object2 <- object1 <- object
+                annotation(object1) <- pkgs[1]
+                annotation(object2) <- pkgs[2]                
+              
+                tmp1 <- dbGetQuery(db(object1), sql)
+                tmp2 <- dbGetQuery(db(object2), sql)
+                tmp <- rbind(tmp1, tmp2)
+              } else{
+                tmp <- dbGetQuery(db(object), sql)
+              }
+              idx <- match(fs, tmp[["man_fsetid"]])
+              pos <- tmp[idx, "physical_pos"]
+            } else {
+              pos <- featureData(object)$position
+            }
+            return(pos)
           })
+
+##setMethod("position", "SnpLevelSet",
+##          function(object){
+##            require("RSQLite") || stop("RSQLite package not available")            
+##            fs <- featureNames(object)
+##            sql <- "SELECT man_fsetid, physical_pos FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
+##            tmp <- dbGetQuery(db(object), sql)
+##            idx <- match(fs, tmp[["man_fsetid"]])
+##            tmp[idx, "physical_pos"]
+##          })
 
 setMethod("combine", signature=signature(x="SnpLevelSet", y="SnpLevelSet"),
           function(x, y, ...){
