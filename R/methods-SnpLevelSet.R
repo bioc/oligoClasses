611cfd810f4d268cdc9d4beed92b56bba2ae0164
Rob Scharp <rscharpf@jhsph.edu>
2008-06-23 18:45:06 +0000

    updated setValidity methods for snp-level set objects
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@32400 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-SnpLevelSet.R
+++ R/methods-SnpLevelSet.R
@@ -57,28 +57,28 @@ setReplaceMethod("chromosome", c("AnnotatedDataFrame", "character"),
 
 setMethod("position", "SnpLevelSet",
           function(object){
-            if(!("position" %in% fvarLabels(object))){
-              require("RSQLite") || stop("RSQLite package not available")
-              fs <- featureNames(object)
-              sql <- "SELECT man_fsetid, physical_pos FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
-              pkgs <- strsplit(annotation(object), ",")[[1]]
-              if(length(pkgs) > 1){
-                object2 <- object1 <- object
-                annotation(object1) <- pkgs[1]
-                annotation(object2) <- pkgs[2]                
+		  if(!("position" %in% fvarLabels(object))){
+			  require("RSQLite") || stop("RSQLite package not available")
+			  fs <- featureNames(object)
+			  sql <- "SELECT man_fsetid, physical_pos FROM featureSet WHERE man_fsetid LIKE 'SNP%'"
+			  pkgs <- strsplit(annotation(object), ",")[[1]]
+			  if(length(pkgs) > 1){
+				  object2 <- object1 <- object
+				  annotation(object1) <- pkgs[1]
+				  annotation(object2) <- pkgs[2]                
               
-                tmp1 <- dbGetQuery(db(object1), sql)
-                tmp2 <- dbGetQuery(db(object2), sql)
-                tmp <- rbind(tmp1, tmp2)
-              } else{
-                tmp <- dbGetQuery(db(object), sql)
-              }
-              idx <- match(fs, tmp[["man_fsetid"]])
-              pos <- tmp[idx, "physical_pos"]
-            } else {
-              pos <- featureData(object)$position
-            }
-            return(pos)
+				  tmp1 <- dbGetQuery(db(object1), sql)
+				  tmp2 <- dbGetQuery(db(object2), sql)
+				  tmp <- rbind(tmp1, tmp2)
+			  } else{
+				  tmp <- dbGetQuery(db(object), sql)
+			  }
+			  idx <- match(fs, tmp[["man_fsetid"]])
+			  pos <- tmp[idx, "physical_pos"]
+		  } else {
+			  pos <- featureData(object)$position
+		  }
+		  return(pos)
           })
 
 ##setMethod("position", "SnpLevelSet",
