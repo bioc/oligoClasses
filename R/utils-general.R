8c777f72a7fd02d7d29427a34bfc95113304107e
Benilton Carvalho <bcarvalh@jhsph.edu>
2010-02-22 13:06:45 +0000

    Updated contact info / combined utils and functions in utils-general.R / added item to TODO
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@44786 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils-general.R
+++ R/utils-general.R
@@ -1,3 +1,53 @@
+checkAnnotation <- function(x){
+	if(length(x) == 0) return(FALSE)
+	x <- strsplit(x, ",")[[1]]
+	if(length(x) == 1){
+		return(isSupportedAnnotation(x))
+	}
+	if(length(x) == 2)
+		return(all(sapply(x, isSupportedAnnotation)))
+}
+
+loader <- function(theFile, envir, pkgname){
+	theFile <- file.path(system.file(package=pkgname),
+			     "extdata", theFile)
+	if (!file.exists(theFile))
+		stop("File ", theFile, " does not exist in ", pkgname)
+	load(theFile, envir=envir)
+}
+
+requireAnnotation <- function(pkgname, lib=.libPaths()[1], verbose=TRUE){
+  stopifnot(is.character(pkgname), !missing(pkgname))
+  status <- require(pkgname, character.only=TRUE, quietly=!verbose)
+  if (!status)
+    status <- pdPkgFromBioC(pkgname, lib=lib, verbose=verbose)
+  status
+}
+
+isSupportedAnnotation <- function(x){
+	validAnn <- supportedAnnotation()
+	x %in% validAnn
+}
+
+supportedAnnotation <- function(){
+	c("pd.mapping50k.hind240", "pd.mapping50k.xba240",
+	  "pd.mapping50k.hind240,pd.mapping50k.xba240",	  
+	  "pd.mapping250k.nsp",
+	  "pd.mapping250k.sty",
+	  "pd.mapping250k.nsp,pd.mapping250k.sty",
+	  "pd.genomewidesnp.5",
+	  "pd.genomewidesnp.6",
+	  "genomewidesnp6",
+	  "genomewidesnp5",
+	  "human370v1c",
+	  "human370quadv3c",
+	  "human550v3b",
+	  "human650v3a",
+	  "human610quadv1b",
+	  "human660quadv1a",
+	  "human1mduov3b")	  
+}
+
 affyPlatforms <- function(){
   platforms <- c("pd.mapping50k.xba240",
                  "pd.mapping50k.hind240",
