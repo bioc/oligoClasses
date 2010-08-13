8b8a09fdd94e4aeee2e613eb0db166e008fbbabc
Rob Scharp <rscharpf@jhsph.edu>
2010-08-13 03:21:18 +0000

    moved totalCopyNumber to crlmm package
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@48783 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -1,45 +1,6 @@
-
-
 ##setReplaceMethod("CB", "CNSet", function(object, value) assayDataElementReplace(object, "CB", value))
 ##setReplaceMethod("CA", "CNSet", function(object, value) assayDataElementReplace(object, "CA", value))
 
-setMethod("totalCopyNumber",
-	  signature=signature(object="CNSet", i="integerOrMissing", j="integerOrMissing"),
-	  function(object, i, j, ...){
-	if(missing(i) & missing(j)){
-		if(inherits(CA(object), "ff") | inherits(CA(object), "ffdf")) stop("Must specify i and/or j for ff objects")
-	}
-	if(missing(i) & !missing(j)){
-		snp.index <- which(isSnp(object))	
-		cn.total <- as.matrix(CA(object)[, j])
-		if(length(snp.index) > 0){
-			cb <- as.matrix(CB(object)[snp.index, j])
-			snps <- (1:nrow(cn.total))[i %in% snp.index]
-			cn.total[snps, ] <- cn.total[snps, j] + cb				
-		}
-	}
-	if(!missing(i) & missing(j)){
-		snp.index <- intersect(which(isSnp(object)), i)
-		cn.total <- as.matrix(CA(object)[i, ])
-		if(length(snp.index) > 0){
-			cb <- as.matrix(CB(object)[snp.index, ])
-			snps <- (1:nrow(cn.total))[i %in% snp.index]
-			cn.total[snps, ] <- cn.total[snps, ] + cb				
-		}
-	}
-	if(!missing(i) & !missing(j)){
-		snp.index <- intersect(which(isSnp(object)), i)		
-		cn.total <- as.matrix(CA(object)[i, j])
-		if(length(snp.index) > 0){
-			cb <- as.matrix(CB(object)[snp.index, j])
-			snps <- (1:nrow(cn.total))[i %in% snp.index]
-			cn.total[snps, ] <- cn.total[snps, ] + cb
-		}
-	}
-	cn.total <- cn.total/100
-	dimnames(cn.total) <- NULL
-	return(cn.total)
-})
 
 
 
