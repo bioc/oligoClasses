73a8743b5caa5ce18ac0e9c49a95ce75aaa9a58d
Rob Scharp <rscharpf@jhsph.edu>
2010-08-21 02:45:14 +0000

    added methods for AssayData and a Rd file for  assay data methods
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@48918 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-AssayData.R
@@ -0,0 +1,76 @@
+setMethod("batchNames", "AssayData",
+	  function(object){
+		  ##should call method for AssayData
+		  sampleNames(object)
+	  })
+
+setReplaceMethod("batchNames", "AssayData",
+		 function(object, value){
+			 sampleNames(object) <- value
+			 return(object)
+		 })
+
+setMethod("nu", c("AssayData", "character"),
+	  function(object, allele){
+		  getValue <- function(allele){
+			  switch(allele,
+				 A="nuA",
+				 B="nuB",
+				 stop("allele must be 'A' or 'B'"))
+		  }
+		  val <- getValue(allele)
+		  assayDataElement(object, val)
+})
+
+
+
+setMethod("phi", c("AssayData", "character"),
+	  function(object, allele){
+		  getValue <- function(allele){
+			  switch(allele,
+				 A="phiA",
+				 B="phiB",
+				 stop("allele must be 'A' or 'B'"))
+		  }
+		  val <- getValue(allele)
+		  assayDataElement(object, val)
+	  })
+
+setMethod("sigma2", c("AssayData", "character"),
+	  function(object, allele){
+		  getValue <- function(allele){
+			  switch(allele,
+				 A="sig2A",
+				 B="sig2B",
+				 stop("allele must be 'A' or 'B'"))
+		  }
+		  val <- getValue(allele)
+		  assayDataElement(object, val)
+	  })
+
+##setMethod("tau2", c("AssayData", "character"),
+##	  function(object, allele){
+##		  getValue <- function(allele){
+##			  switch(allele,
+##				 A="tau2A",
+##				 B="tau2B",
+##				 stop("allele must be 'A' or 'B'"))
+##		  }
+##		  val <- getValue(allele)
+##		  assayDataElement(object, val)
+##	  })
+##
+##setMethod("corr", c("AssayData", "character"),
+##	  function(object, allele){
+##		  getValue <- function(allele){
+##			  switch(allele,
+##				 AA="corrAA",
+##				 AB="corrAB",
+##				 BB="corrBB",
+##				 stop("allele must be 'AA', 'AB', or 'BB'"))
+##		  }
+##		  val <- getValue(allele)
+##		  assayDataElement(object, val)
+##	  })
+
+setMethod("flags", signature(object="AssayData"), function(object) assayDataElement(object, "flags"))
