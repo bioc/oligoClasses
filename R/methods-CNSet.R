8585c64a23ed382fa25aabe2d7e688fb3ac529d1
Rob Scharp <rscharpf@jhsph.edu>
2011-09-16 02:23:15 +0000

    Remove commented code from methods-CNSet
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@58085 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -85,24 +85,6 @@ setReplaceMethod("batch", signature=signature(object="CNSet"),
 		 object
 })
 
-##assayDataElement <- function(object, elt) assayData(object)[[elt]]
-##assayDataElementReplace <- function(obj, elt, value) {
-##    storage.mode <- storageMode(obj)
-##    switch(storageMode(obj),
-##           "lockedEnvironment" = {
-##               aData <- copyEnv(assayData(obj))
-##               if (is.null(value)) rm(list=elt, envir=aData)
-##               else aData[[elt]] <- value
-##               assayDataEnvLock(aData)
-##               assayData(obj) <- aData
-##           },
-##           "environment" = {
-##               if (is.null(value)) rm(list=elt, envir=assayData(obj))
-##               else assayData(obj)[[elt]] <- value
-##           },
-##           list = assayData(obj)[[elt]] <- value)
-##    obj
-##}
 
 setMethod("batchNames", "CNSet", function(object)  batchNames(batchStatistics(object)))
 
@@ -175,85 +157,11 @@ setMethod("close", "matrix", function(con, ...) return(con))
 setMethod("close", "array", function(con, ...) return(con))
 
 
-##setMethod("lM", "CNSet", function(object){
-####	bs <- batchStatistics(object)
-####	elem.names <- ls(bs)
-####	param.names <- c("tau2A.AA", "tau2A.BB", "tau2B.AA", "tau2B.BB",
-####			 "nuA", "nuB", "phiA", "phiB",
-####			 "phiPrimeA", "phiPrimeB", "corrAB", "corrBB", "corrAA")
-####	index <- match(param.names, elem.names)
-####	return(bs[index])
-##})
-##setReplaceMethod("lM", signature=signature(object="CNSet", value="AssayData"),
-##		 function(object, value){
-##			 object@lM <- value
-##			 object
-##		 })
-
-setMethod("batchStatistics", signature=signature(object="CNSet"), function(object) object@batchStatistics)
-setReplaceMethod("batchStatistics", signature=signature(object="CNSet", value="AssayData"),
-	 function(object, value){
-		 object@batchStatistics <- value
-		 object
-	 })
-
-##setMethod("Ns", signature=signature(object="CNSet"),
-##	  function(object, batchname){
-##		  if(missing(batchnames)) {
-##			  stop("must specify batchname")
-##		  } else {
-##			  if(!batchname %in% batchNames(object))
-##				  stop(paste("element.name must be one of ",  batchNames(object)))
-##			  Ns <- matrix(NA, nrow(object), 3)
-##			  colnames(NS) <- c("AA", "AB", "BB")
-##			  j <- match(batchname, batchNames(object))
-##			  Ns[, 1] <- assayDataElement(batchStatistics(object), "N.AA")[, j]
-##			  Ns[, 2] <- assayDataElement(batchStatistics(object), "N.AB")[, j]
-##			  Ns[, 3] <- assayDataElement(batchStatistics(object), "N.BB")[, j]
-##			  return(Ns)
-##		  }
-##	  })
-##setMethod("medians", signature=signature(object="CNSet"),
-##	  function(object, batchname){
-##		  if(missing(batchnames)) {
-##			  stop("must specify batchname")
-##		  } else {
-##			  if(!batchname %in% batchNames(object))
-##				  stop(paste("element.name must be one of ",  batchNames(object)))
-##			  Ns <- matrix(NA, nrow(object), 3)
-##			  colnames(NS) <- c("AA", "AB", "BB")
-##			  j <- match(batchname, batchNames(object))
-##			  Ns[, 1] <- assayDataElement(batchStatistics(object), "median.AA")[, j]
-##			  Ns[, 2] <- assayDataElement(batchStatistics(object), "median.AB")[, j]
-##			  Ns[, 3] <- assayDataElement(batchStatistics(object), "median.BB")[, j]
-##			  return(Ns)
-##		  }
-##	  })
-##setMethod("mads", signature=signature(object="CNSet"),
-##	  function(object, batchname){
-##		  if(missing(batchnames)) {
-##			  stop("must specify batchname")
-##		  } else {
-##			  if(!batchname %in% batchNames(object))
-##				  stop(paste("element.name must be one of ",  batchNames(object)))
-##			  Ns <- matrix(NA, nrow(object), 3)
-##			  colnames(NS) <- c("AA", "AB", "BB")
-##			  j <- match(batchname, batchNames(object))
-##			  Ns[, 1] <- assayDataElement(batchStatistics(object), "mad.AA")[, j]
-##			  Ns[, 2] <- assayDataElement(batchStatistics(object), "mad.AB")[, j]
-##			  Ns[, 3] <- assayDataElement(batchStatistics(object), "mad.BB")[, j]
-##			  return(Ns)
-##		  }
-##	  })
 
 setMethod("nu", c("CNSet", "character"), function(object, allele) nu(batchStatistics(object), allele))
 setMethod("phi", c("CNSet", "character"), function(object, allele) phi(batchStatistics(object), allele))
-##setMethod("phiPrime", c("CNSet", "character"), function(object, allele) phiPrime(batchStatistics(object), allele))
 setMethod("sigma2", c("CNSet", "character"), function(object, allele) sigma2(batchStatistics(object), allele))
-##setMethod("tau2", c("CNSet", "character"), function(object, allele) tau2(batchStatistics(object), allele))
-##setMethod("corr", c("CNSet", "character"), function(object, allele) corr(batchStatistics(object), allele))
 setMethod("flags", signature(object="CNSet"), function(object) flags(batchStatistics(object)))
-
 
 setAs("CNSetLM", "CNSet", function(from){
 	if("batch" %in% varLabels(protocolData(from))){
@@ -308,10 +216,6 @@ setAs("CNSetLM", "CNSet", function(from){
 			    corrAA=lm[["corrAA"]],
 			    corrBB=lm[["corrBB"]],
 			    flags=initializeBigMatrix("flags", nrow(from), length(unique(btch))))
-	##initialize container for Ns, but do not populate.
-##	nr <- sum(fData(from)$isSnp, na.rm=TRUE)
-##	Ns <- initializeNumberGenotypeFrom(from, unique(as.character(btch)), nr)
-##	Ns <- initializeNumberGenotypeFrom(from, unique(as.character(btch)))
 	obj <- new("CNSet",
 		   alleleA=assayData(from)[["alleleA"]],
 		   alleleB=assayData(from)[["alleleB"]],
@@ -326,123 +230,13 @@ setAs("CNSetLM", "CNSet", function(from){
 	return(obj)
 })
 
-relocateObject <- function(object, to){
-	stopifnot(isPackageLoaded("ff"))
-	is.ff <- function(x) class(x)[1] == "ff_matrix"
-	is.ffdf <- function(x) is(x, "ffdf")
-	##AssayData
-	storage.mode <- storageMode(assayData(object))
-	orig <- assayData(object)
-	physical <- get("physical")
-	filename <- get("filename")
-	pattern <- get("pattern")
-	f2 <- function(X, dirname){
-		##browser()
-		X$filename <- file.path(dirname, basename(filename(X)))
-		X$pattern <- file.path(dirname, basename(pattern(X)))
-		physical(X)$filename <- file.path(dirname, basename(filename(X)))
-		physical(X)$pattern <- file.path(dirname, basename(pattern(X)))
-		X
-	}
-	assayData(object) <-
-		switch(storage.mode,
-		       environment=,
-		       lockedEnvironment={
-			       aData <- new.env(parent=emptyenv())
-			       for (nm in ls(orig)) {
-				       obj <- orig[[nm]]
-				       if (is.ff(obj)) {
-					       physical(obj)$pattern <- file.path(to, basename(pattern(obj)))
-					       physical(obj)$filename = file.path(to, basename(filename(obj)))
-				       }
-				       else if (is.ffdf(obj)) {
+setMethod("batchStatistics", signature=signature(object="CNSet"), function(object) object@batchStatistics)
+setReplaceMethod("batchStatistics", signature=signature(object="CNSet", value="AssayData"),
+	 function(object, value){
+		 object@batchStatistics <- value
+		 object
+	 })
 
-					       ##important to not assign this to obj.  A list is returned (not a ffdf object, but the pointer is changed)
-					       lapply(physical(obj), f2, dirname=to)
-				       }
-				       else message("Unable to change filename and pattern for assayData.")
-				       aData[[nm]] <- obj
-			       }
-			       if ("lockedEnvironment" == storage.mode) Biobase:::assayDataEnvLock(aData)
-			       aData
-		       },
-					#haven't tested yet when storage.mode returns list.
-		       list = {
-			       relocate.fxn <- function(obj,to){
-				       obj <- orig[[nmm]]
-				       filename(obj) <- file.path(to, basename(filename(obj)))
-				       physical(obj)$pattern <- file.path(to, basename(pattern(obj)))
-				       return(obj)
-			       }
-			       lapply(orig, relocate.fxn, to=to)
-		       }
-		       ) # ends switch
-	storage.mode <- storage.mode(batchStatistics(object))
-	orig <- batchStatistics(object)
-	batchStatistics(object) <-
-		switch(storage.mode,
-		       environment =,
-		       lockedEnvironment = {
-			       aData <- new.env(parent=emptyenv())
-			       for(nm in ls(orig)){
-				       obj <- orig[[nm]]
-				       if (is.ff(obj)){
-					       physical(obj)$pattern = file.path(to, basename(pattern(obj)))
-					       physical(obj)$filename = file.path(to, basename(filename(obj)))
-				       }
-				       else if (is.ffdf(obj)) {
-					       f2 <- function(X, dirname){
-						       X$filename <- file.path(dirname, basename(filename(X)))
-						       X$pattern <- file.path(dirname, basename(pattern(X)))
-						       physical(X)$filename <- file.path(dirname, basename(filename(X)))
-						       physical(X)$pattern <- file.path(dirname, basename(pattern(X)))
-						       X
-					       }
-					       ##important to not assign this to obj.  A list is returned (not a ffdf object, but the pointer is changed)
-					       lapply(physical(obj), f2, dirname=to)
-##					       fff <- function(X,dirname){
-##						       physical(X)$pattern = file.path(dirname, basename(physical(X)$pattern))
-##						       physical(X)$filename = file.path(dirname, basename(physical(X)$filename))
-##						       return(X)
-##					       }
-##					       obj2 = lapply(obj, fff, dirname=to)
-                                 }
-                                 else message("Unable to change filename and pattern for batchStatistics.")
-                                 aData[[nm]] <- obj
-                             }
-                             if ("lockedEnvironment" == storage.mode) Biobase:::assayDataEnvLock(aData)
-                             aData
-                         },
-                         ## haven't tested yet when storage.mode returns list
-                         list = {
-                             relocate.fxn <- function(obj, to){
-                                 obj <- orig[[nm]]
-                                 filename(obj) <- file.path(to, basename(filename(obj)))
-                                 physical(obj)$pattern <- file.path(to, basename(pattern(obj)))
-                                 return(obj)
-                             }
-                             lapply(orig, relocate.fxn, to=to)
-                         }
-                         ) # ends switch
-              ##Finally, remove any ff objects that might be in the phenodata
-              tmp <- object$SNR
-              if(is.ff(tmp)){
-                  physical(tmp)$filename <- file.path(to, basename(filename(tmp)))
-                  physical(tmp)$pattern <- file.path(to, basename(pattern(tmp)))
-                  SNR <- tmp
-                  tmp <- object$SKW
-                  physical(tmp)$filename <- file.path(to, basename(filename(tmp)))
-                  physical(tmp)$pattern <- file.path(to, basename(pattern(tmp)))
-                  SKW <- tmp
-                  pD <- new("AnnotatedDataFrame", data=data.frame(list(SNR=SNR[,],
-                                                  SKW=SKW[,],
-                                                  gender=object$gender)),
-                            varMetadata=data.frame(labelDescription=c("SNR", "SKW", "gender"), row.names=c("SNR", "SKW", "gender")))
-                  sampleNames(pD) <- sampleNames(object)
-                  phenoData(object) <- pD
-              }
-              object
-}
 
 
 
