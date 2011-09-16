939cd9d4139b7de50119badc037563d23d7b9ee8
Rob Scharp <rscharpf@jhsph.edu>
2011-09-16 02:22:25 +0000

    Modify '[' method for CNSet -- do not check class for 'ff' prior to callNext method
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@58077 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -27,11 +27,12 @@ setMethod("show", "CNSet", function(object){
 })
 
 setMethod("[", "CNSet", function(x, i, j, ..., drop=FALSE){
-	isff <- is(A(x), "ff") | is(A(x), "ffdf")
-	if(isff) open(x)
-	xx <- callNextMethod(x, i, j, ..., drop=FALSE)
-	if(isff) close(x)
-	x <- xx; rm(x)
+	##isff <- is(A(x), "ff") | is(A(x), "ffdf")
+	##if(isff) open(x)
+	open(x)
+	x <- callNextMethod(x, i, j, ..., drop=FALSE)
+##	if(isff) close(x)
+##	x <- xx; rm(x)
 	## ensure that assayData elements are matrices after subset operation
 	isdf <- is(A(x), "data.frame")
 	if(isdf){
