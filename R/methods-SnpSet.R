0882e04b0dabf3f45c8647e1dad36547b47e1447
Rob Scharp <rscharpf@jhsph.edu>
2011-09-27 02:27:27 +0000

    Edit method for coercing oligoSnpSet to a data.frame for plotting
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@58440 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-SnpSet.R
+++ R/methods-SnpSet.R
@@ -87,6 +87,7 @@ setMethod("findOverlaps", signature(query="RangedDataCNV", subject="AnnotatedDat
 		  }
 		  start <- start(query)
 		  end <- end(query)
+
 		  CHR <- chromosome(query)
 		  ##featuresInXlim(object, start=start(range), end=end(range), CHR=range$chrom, ...)
 		  if("frame" %in% names(list(...))) {
