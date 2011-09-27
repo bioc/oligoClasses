2eaf0feea793bacc0773b6bf618051e9307c3446
Rob Scharp <rscharpf@jhsph.edu>
2011-09-27 02:28:03 +0000

    Add generic featuresInRange and method for SnpSet
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@58446 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -58,4 +58,3 @@ setGeneric("state", function(object) standardGeneric("state"))
 
 setGeneric("featuresInRange", function(object, range, FRAME=0, FRAME.LEFT, FRAME.RIGHT, ...)
 	   standardGeneric("featuresInRange"))
-
