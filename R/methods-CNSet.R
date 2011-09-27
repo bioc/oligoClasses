2126e2be5d7f7c10ce958a073fbdf5dd8db58f54
Rob Scharp <rscharpf@jhsph.edu>
2011-09-27 02:26:56 +0000

    oligoClasses on github
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@58434 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -157,7 +157,6 @@ setMethod("close", "matrix", function(con, ...) return(con))
 setMethod("close", "array", function(con, ...) return(con))
 
 
-
 setMethod("nu", c("CNSet", "character"), function(object, allele) nu(batchStatistics(object), allele))
 setMethod("phi", c("CNSet", "character"), function(object, allele) phi(batchStatistics(object), allele))
 setMethod("sigma2", c("CNSet", "character"), function(object, allele) sigma2(batchStatistics(object), allele))
