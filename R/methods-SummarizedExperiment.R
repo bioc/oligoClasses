5c26a645be0d8480250e837675e341fe56fe4a09
Herve Pages <hpages@fhcrc.org>
2015-05-12 04:24:18 +0000

    Add "baf", "chromosome", "isSnp", and "lrr" methods for RangedSummarizedExperiment objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/oligoClasses@103675 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-SummarizedExperiment.R
+++ R/methods-SummarizedExperiment.R
@@ -1,7 +1,22 @@
+setMethod("chromosome", signature(object="RangedSummarizedExperiment"),
+	  function(object,...) as.character(seqnames(object)))
+setMethod("isSnp", signature(object="RangedSummarizedExperiment"),
+	  function(object,...) values(rowRanges(object))$isSnp)
+setMethod("lrr", signature(object="RangedSummarizedExperiment"),
+	  function(object){
+		  assays(object)[[1]]
+	  })
+setMethod("baf", signature(object="RangedSummarizedExperiment"),
+	  function(object){
+		  assays(object)[[2]]
+	  })
+
+## Remove the methods below once the transition from SummarizedExperiment
+## to RangedSummarizedExperiment is complete. [H. Pages - May 11, 2015]
 setMethod("chromosome", signature(object="SummarizedExperiment"),
 	  function(object,...) as.character(seqnames(object)))
 setMethod("isSnp", signature(object="SummarizedExperiment"),
-	  function(object,...) values(rowData(object))$isSnp)
+	  function(object,...) values(rowRanges(object))$isSnp)
 setMethod("lrr", signature(object="SummarizedExperiment"),
 	  function(object){
 		  assays(object)[[1]]
@@ -10,3 +25,4 @@ setMethod("baf", signature(object="SummarizedExperiment"),
 	  function(object){
 		  assays(object)[[2]]
 	  })
+
